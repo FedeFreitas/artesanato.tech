4b0acb3e081486e2b1bc46f70b8b1e03e5e5aa7d
Felipe de Freitas <defreitas.comercial@gmail.com>
2026-05-23 14:32:52 -0300

    correção: configurando o texto exibido para UTF-8

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const http = require('http');
 
 const server = http.createServer (
     (req,res) => {
-
+        // writeHead monta o cabeçalho da resposta, onde define as configurações do corpo da resposta.
+        // 200 é o status code HTTP informando que deu tudo certo com a chamada.
+        // text/plain quer dizer que a resposta é texto puro e não HTML ou Json.
+        // charset=utf-8 codifica o texto da mensage em UTF-8
+        res.writeHead(200,{'Content-Type':'text/plain; charset=utf-8'})
         res.end('Olá, Mundo!')
 
     }
